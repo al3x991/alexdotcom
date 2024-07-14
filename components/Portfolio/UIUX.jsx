5e4236b79431832024dccea27218f53ad2852ac2
al3x991 <ostrategic@gmail.com>
2024-07-14 15:31:42 +0100

    first commit

--- components/Portfolio/UIUX.jsx
+++ components/Portfolio/UIUX.jsx
@@ -94,7 +94,7 @@ const UIUX = () => {
 
               <div className="content">
                 <p>design</p>
-                <h2>Web 01</h2>
+                <h2>UI/UX 01</h2>
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Labore, neque? Lorem ipsum dolor sit amet consectetur
@@ -128,7 +128,75 @@ const UIUX = () => {
 
               <div className="content">
                 <p>design</p>
-                <h2>Web 02</h2>
+                <h2>UI/UX 02</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Labore, neque? Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Ipsum, ex.
+                </p>
+                <span className="mt-4 flex flex-col items-center justify-start gap-4 space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0">
+                  <Link
+                    href="/assets/file/cv.pdf"
+                    className="rounded-full bg-lime px-3 py-2 text-[16px] font-semibold text-white hover:opacity-[0.9]"
+                  >
+                    Live Demo
+                  </Link>
+                  <Link
+                    href="https://github.com/al3x991"
+                    className="flex items-center gap-2 rounded-full bg-black/20 px-3 py-2 text-[16px] font-semibold text-black duration-300 ease-in-out hover:bg-black/30 dark:bg-white/20 dark:text-white dark:hover:bg-white/30"
+                  >
+                    Github Repo
+                    <FaGithub className="text-[16px] font-bold text-white" />
+                  </Link>
+                </span>
+              </div>
+            </div>
+            <div className="item">
+              <Image
+                width={250}
+                height={180}
+                alt="bg_image"
+                src="/images/img3.jpg"
+              />
+              <div className="overlay absolute left-0 top-0 z-0 h-full w-full bg-black opacity-[0.9]"></div>
+
+              <div className="content">
+                <p>design</p>
+                <h2>UI/UX 03</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Labore, neque? Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Ipsum, ex.
+                </p>
+                <span className="mt-4 flex flex-col items-center justify-start gap-4 space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0">
+                  <Link
+                    href="/assets/file/cv.pdf"
+                    className="rounded-full bg-lime px-3 py-2 text-[16px] font-semibold text-white hover:opacity-[0.9]"
+                  >
+                    Live Demo
+                  </Link>
+                  <Link
+                    href="https://github.com/al3x991"
+                    className="flex items-center gap-2 rounded-full bg-black/20 px-3 py-2 text-[16px] font-semibold text-black duration-300 ease-in-out hover:bg-black/30 dark:bg-white/20 dark:text-white dark:hover:bg-white/30"
+                  >
+                    Github Repo
+                    <FaGithub className="text-[16px] font-bold text-white" />
+                  </Link>
+                </span>
+              </div>
+            </div>
+            <div className="item">
+              <Image
+                width={250}
+                height={180}
+                alt="bg_image"
+                src="/images/img4.jpg"
+              />
+              <div className="overlay absolute left-0 top-0 z-0 h-full w-full bg-black opacity-[0.9]"></div>
+
+              <div className="content">
+                <p>design</p>
+                <h2>UI/UX 04</h2>
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Labore, neque? Lorem ipsum dolor sit amet consectetur
@@ -173,6 +241,24 @@ const UIUX = () => {
                 height={180}
                 alt="bg_image"
                 src="/images/img2.jpg"
+              />
+              <div className="content">Name Slider</div>
+            </div>
+            <div className="item">
+              <Image
+                width={250}
+                height={180}
+                alt="bg_image"
+                src="/images/img3.jpg"
+              />
+              <div className="content">Name Slider</div>
+            </div>
+            <div className="item">
+              <Image
+                width={250}
+                height={180}
+                alt="bg_image"
+                src="/images/img4.jpg"
               />
               <div className="content">Name Slider</div>
             </div>
