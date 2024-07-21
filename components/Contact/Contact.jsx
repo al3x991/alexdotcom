97333a5720dcad61154a814fb12b4d62cef58bf8
Omoruyi Osaretin A <42362272+al3x991@users.noreply.github.com>
2024-07-21 16:09:32 +0000

    addedd some animations

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -4,10 +4,73 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 const Contact = () => {
+  return (
+    <>
+      <section className="">
+        <div className="mx-auto max-w-screen-md px-4 py-8 lg:py-16">
+          <h2 className="text-gray-900 mb-4 text-center text-4xl font-extrabold tracking-tight dark:text-white">
+            Contact Us
+          </h2>
+          <p className="text-gray-500 dark:text-gray-400 mb-8 text-center font-light sm:text-xl lg:mb-16">
+            Got a technical issue? Want to send feedback about a beta feature?
+            Need details about our Business plan? Let us know.
+          </p>
+          <form action="#" className="space-y-8">
+            <div>
+              <label
+                for="email"
+                className="text-gray-900 dark:text-gray-300 mb-2 block text-sm font-medium"
+              >
+                Your email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="bg-gray-50 border-gray-300 text-gray-900 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light block w-full rounded-lg border p-2.5 text-sm shadow-sm dark:text-white"
+                placeholder="name@flowbite.com"
+                required
+              />
+            </div>
+            <div>
+              <label
+                for="subject"
+                className="text-gray-900 dark:text-gray-300 mb-2 block text-sm font-medium"
+              >
+                Subject
+              </label>
+              <input
+                type="text"
+                id="subject"
+                className="text-gray-900 bg-gray-50 border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light block w-full rounded-lg border p-3 text-sm shadow-sm dark:text-white"
+                placeholder="Let us know how we can help you"
+                required
+              />
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                for="message"
+                className="text-gray-900 dark:text-gray-400 mb-2 block text-sm font-medium"
+              >
+                Your message
+              </label>
+              <textarea
+                id="message"
+                rows="6"
+                className="text-gray-900 bg-gray-50 border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500 block w-full rounded-lg border p-2.5 text-sm shadow-sm dark:text-white"
+                placeholder="Leave a comment..."
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="bg-primary-700 hover:bg-primary-800 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 rounded-lg px-5 py-3 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 sm:w-fit"
+            >
+              Send message
+            </button>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
 
-    return (
-        <></>
-    )
-}
-  
 export default Contact;
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,9 +1,52 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { init } from "ityped";
+import Circle from "@/components/AnimCircle/Circle";
+import { useEffect, useRef, useState } from "react";
 import { MdOutlineFileDownload } from "react-icons/md";
 import { FaGithub } from "react-icons/fa";
 
+const useCountUp = (endValue) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    let start = 0;
+    const duration = 2000; // duration of the animation in milliseconds
+    const increment = endValue / (duration / 10); // calculate the increment
+
+    const counter = setInterval(() => {
+      start += increment;
+      if (start >= endValue) {
+        start = endValue;
+        clearInterval(counter);
+      }
+      setCount(Math.ceil(start));
+    }, 10); // update every 10ms
+
+    return () => clearInterval(counter);
+  }, [endValue]);
+
+  return count;
+};
+
 const Hero = () => {
+  const textRef = useRef();
+  const yearsOfExperience = useCountUp(6);
+  const projectsCompleted = useCountUp(21);
+  const technologiesMastered = useCountUp(7);
+  const codeCommits = useCountUp(307);
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Web Development.", "Mobile Development."],
+    });
+  }, []);
+
   return (
     <>
       <section
@@ -12,7 +55,7 @@ const Hero = () => {
       >
         <div className="container">
           <div className="mx-4  flex flex-wrap">
-            <div className="flex flex-col justify-center gap-6  px-4 lg:flex-row">
+            <div className="flex flex-col justify-between gap-12 px-4 lg:flex-row">
               <div
                 className="wow fadeInUp order-2 mx-auto flex max-w-[620px] flex-col justify-evenly lg:order-1"
                 data-wow-delay=".2s"
@@ -25,10 +68,10 @@ const Hero = () => {
                   <span className=" text-lime">Alexander Omoruyi</span>
                 </h1>
                 <p className="text-md mb-6 font-light !leading-relaxed text-body-color dark:text-white dark:opacity-90">
-                  I excel at developing scalable software solutions that
-                  transform business needs into efficient applications. Reliable
-                  and communicative, I am passionate about advancing web
-                  development.
+                  I excel at developing software solutions that transform
+                  business needs into efficient applications. Reliable and
+                  communicative, I am passionate about advancing{" "}
+                  <span ref={textRef}></span>
                 </p>
                 <div className="flex flex-col items-center justify-start gap-10 space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
@@ -48,39 +91,49 @@ const Hero = () => {
                 </div>
               </div>
 
-              <div className="relative order-1 mx-auto h-56 w-56 rounded-full lg:order-2 lg:mx-0 lg:h-64 lg:w-64">
-                <Image
+              <div className="relative order-1 mx-auto h-56 w-56 rounded-full lg:order-2 lg:mx-0 lg:ml-[30px] lg:h-64 lg:w-64">
+                {/* <Image
                   className="rounded-full"
                   fill
                   src="/images/hero/alex.jpg"
                   alt="image description"
-                />
+                />  */}
+
+                <Circle />
               </div>
             </div>
             <div className="ites-center mx-4 mb-12 mt-12 grid w-full  max-w-[900px] grid-cols-1 justify-between gap-4 md:grid-cols-2  md:gap-0 lg:grid-cols-4">
               <div className="flex items-center justify-center gap-2">
-                <h2 className=" text-[44px] font-bold text-white">6</h2>
+                <h2 className="text-[44px] font-bold text-white">
+                  {yearsOfExperience}
+                </h2>
                 <span className="w-[80px] text-base font-normal leading-[16px] text-white opacity-[0.7]">
                   Years of experience
                 </span>
               </div>
 
               <div className="flex items-center justify-center gap-2">
-                <h2 className=" text-[44px] font-bold text-white">21</h2>
+                <h2 className="text-[44px] font-bold text-white">
+                  {projectsCompleted}
+                </h2>
                 <span className="w-[80px] text-base font-normal leading-[16px] text-white opacity-[0.7]">
                   Projects Completed
                 </span>
               </div>
 
               <div className="flex items-center justify-center gap-2">
-                <h2 className=" text-[44px] font-bold text-white">7</h2>
+                <h2 className="text-[44px] font-bold text-white">
+                  {technologiesMastered}
+                </h2>
                 <span className="w-[80px] text-base font-normal leading-[16px] text-white opacity-[0.7]">
                   Technologies mastered
                 </span>
               </div>
 
               <div className="flex items-center justify-center gap-2">
-                <h2 className=" text-[44px] font-bold text-white">307</h2>
+                <h2 className="text-[44px] font-bold text-white">
+                  {codeCommits}
+                </h2>
                 <span className="w-[80px] text-base font-normal leading-[16px] text-white opacity-[0.7]">
                   Code commits
                 </span>
