96beaf088ca5271ceb544b82b8fdce467602440b
al3x991 <ostrategic@gmail.com>
2024-08-07 12:27:48 +0100

    updated

--- components/Portfolio/Web.jsx
+++ components/Portfolio/Web.jsx
@@ -1,293 +1,112 @@
-// components/Portfolio/Web.js
 "use client";
 
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
-import React, { useEffect, useState } from "react";
-import Script from "next/script";
+import Link from "next/link";
+import { motion } from "framer-motion";
 import "./styles.css";
 
 const Web = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const slides = [
+    {
+      src: "/images/web01.webp",
+      title: "TitanTTrade",
+      description: "A web3 crypto investment and trading platform built using the Next.js framework. The application provides a seamless and secure environment for users to connect their Bitcoin, Ethereum, or Solana wallets and start trading.",
+      demoLink: "https://block-app-psi.vercel.app/",
+      repoLink: "https://github.com/al3x991/Titanttrade",
+    },
+    {
+      src: "/images/web2.webp",
+      title: "EthosMatrix",
+      description: "A sleek platform for cryptocurrency investment.",
+      demoLink: "https://ethosmatrix.com",
+      repoLink: "https://github.com/al3x991/EthosMatrixWebsite",
+    },
+    {
+      src: "/images/web3.webp",
+      title: "Movie (UI CLONE)",
+      description: "A sleek video streaming platform inspired by Netflix, offering a seamless and secure experience for discovering and enjoying your favorite movies and TV shows.",
+      demoLink: "https://vibrant-kirch-c658a7.netlify.app/",
+      repoLink: "https://github.com/al3x991/TheMovieApp",
+    },
+    {
+      src: "/images/web4.webp",
+      title: "ShopNow",
+      description: "An online shopping platform.",
+      demoLink: "https://sad-tereshkova-ee89e8.netlify.app/",
+      repoLink: "https://github.com/al3x991/ShopNow/tree/master",
+    },
+  ];
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + slides.length) % slides.length);
+  };
+
+  const handleThumbnailClick = (index) => {
+    setCurrentIndex(index);
+  };
+
   useEffect(() => {
-    // Slider script initialization
-    let items = document.querySelectorAll(".slider .list .item");
-    let next = document.getElementById("next");
-    let prev = document.getElementById("prev");
-    let thumbnails = document.querySelectorAll(".thumbnail .item");
-
-    let countItem = items.length;
-    let itemActive = 0;
-    let refreshInterval = null;
-
-    function showSlider() {
-      let itemActiveOld = document.querySelector(".slider .list .item.active");
-      let thumbnailActiveOld = document.querySelector(
-        ".thumbnail .item.active"
-      );
-      itemActiveOld.classList.remove("active");
-      thumbnailActiveOld.classList.remove("active");
-
-      items[itemActive].classList.add("active");
-      thumbnails[itemActive].classList.add("active");
-
-      clearInterval(refreshInterval);
-      refreshInterval = setInterval(() => {
-        next.click();
-      }, 5000);
-    }
-
-    next.onclick = function () {
-      itemActive = itemActive + 1;
-      if (itemActive >= countItem) {
-        itemActive = 0;
-      }
-      showSlider();
-    };
-
-    prev.onclick = function () {
-      itemActive = itemActive - 1;
-      if (itemActive < 0) {
-        itemActive = countItem - 1;
-      }
-      showSlider();
-    };
-
-    thumbnails.forEach((thumbnail, index) => {
-      thumbnail.addEventListener("click", () => {
-        itemActive = index;
-        showSlider();
-      });
-    });
-
-    // Initial auto run slider
-    refreshInterval = setInterval(() => {
-      next.click();
-    }, 10000);
-
-    // Cleanup on component unmount
-    return () => {
-      clearInterval(refreshInterval);
-      next.onclick = null;
-      prev.onclick = null;
-      thumbnails.forEach((thumbnail, index) => {
-        thumbnail.removeEventListener("click", () => {
-          itemActive = index;
-          showSlider();
-        });
-      });
-    };
-  }, []); // Empty dependency array ensures this effect runs only once on mount
+    const interval = setInterval(handleNext, 5000);
+    return () => clearInterval(interval);
+  }, [currentIndex]);
 
   return (
-    <>
-      <section id="portfolio" className="relative z-10 h-full lg:max-h-screen">
-        <div className="slider mb-[50px] mt-[0px] h-screen md:mb-[0px] md:mt-[-150px]">
-          <div className="list">
-            <div className="item active">
-              <Image
-                width={1920}
-                height={1080}
-                alt="bg_image"
-                src="/images/web01.webp"
-              />
-              <div className="overlay absolute left-0 top-0 z-0 h-full w-full bg-black opacity-[0.9]"></div>
-
-              <div className="content top-[23%] scale-90  md:top-[45.5%]  md:max-w-[80%] lg:left-[6.5%] lg:top-[49%] lg:w-[500px] lg:scale-100">
-                <div className="max-w-lg">
-                <p>Web Development</p>
-                <h2>TitanTTrade</h2>
-                <p>
-                 A web3 crypto investment and trading platform built using the Next.js framework. The application provides a seamless and secure environment for users to connect their Bitcoin, Ethereum, or Solana wallets and start trading.
-                </p>
-                <span className="mt-4 flex items-center justify-start gap-4 space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0">
-                  <Link
-                  target="_blank"
-                    href="https://block-app-psi.vercel.app/"
-                    className="rounded-full bg-lime px-3 py-2 text-[16px] font-semibold text-white hover:opacity-[0.9]"
-                  >
-                    Live Demo
-                  </Link>
-                  <Link
-                  target="_blank"
-                    href="https://github.com/al3x991/Titanttrade"
-                    className="flex items-center gap-2 rounded-full bg-black/20 px-3 py-2 text-[16px] font-semibold text-black duration-300 ease-in-out hover:bg-black/30 dark:bg-white/20 dark:text-white dark:hover:bg-white/30"
-                  >
-                    Github Repo
-                    <FaGithub className="text-[16px] font-bold text-white" />
-                  </Link>
-                </span>
-                </div>
+    <section className="slider-container">
+      <div className="slider">
+        {slides.map((slide, index) => (
+          <motion.div
+            key={index}
+            className={`slide ${currentIndex === index ? 'active' : ''}`}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: currentIndex === index ? 1 : 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Image
+              src={slide.src}
+              layout="fill"
+              objectFit="cover"
+              fill
+              alt={`Slide ${index}`}
+            />
+            <div className="overlay"></div>
+            <div className="content top-[23%] max-w-[385px] scale-90 left-[3vw]  sm:left-[10vw] text-center md:text-start py-0 px-[30px] md:left-[6vw]   md:top-[30.5%]  md:max-w-[45%]  lg:left-[7vw] lg:top-[35vh]  lg:scale-100">
+              <h2>{slide.title}</h2>
+              <span>{slide.description}</span>
+              <div className="mt-4 flex items-center justify-start gap-4 space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0">
+                <Link href={slide.demoLink} target="_blank" rel="noopener noreferrer" className="rounded-full bg-lime px-3 py-2 text-[16px] font-semibold text-white hover:opacity-[0.9]">Live Demo</Link>
+                <Link href={slide.repoLink} target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 rounded-full bg-black/20 px-3 py-2 text-[16px] font-semibold text-black duration-300 ease-in-out hover:bg-black/30 dark:bg-white/20 dark:text-white dark:hover:bg-white/3">Github Repo <FaGithub className="text-[16px] font-bold text-white" /></Link>
               </div>
             </div>
-            <div className="item">
-              <Image
-                width={1920}
-                height={1080}
-                alt="bg_image"
-                src="/images/web2.webp"
-              />
-              <div className="overlay absolute left-0 top-0 z-0 h-full w-full bg-black opacity-[0.9]"></div>
-
-              <div className="content top-[23%] scale-90  md:top-[45.5%]  md:max-w-[80%] lg:left-[6.5%] lg:top-[49%] lg:w-[500px] lg:scale-100">
-                <div className=" max-w-lg">
-                <p>Web Development</p>
-                <h2>EthosMatrix</h2>
-                <p>
-                 A web3 crypto investment and trading platform built using the Next.js framework. The application provides a seamless and secure environment for users to connect their Bitcoin, Ethereum, or Solana wallets and start trading.
-                </p>
-                <span className="mt-4 flex items-center justify-start gap-4 space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0">
-                  <Link
-                  target="_blank"
-                    href="https://ethosmatrix.com"
-                    className="rounded-full bg-lime px-3 py-2 text-[16px] font-semibold text-white hover:opacity-[0.9]"
-                  >
-                    Live Demo
-                  </Link>
-                  <Link
-                  target="_blank"
-                    href="https://github.com/al3x991/EthosMatrixWebsite"
-                    className="flex items-center gap-2 rounded-full bg-black/20 px-3 py-2 text-[16px] font-semibold text-black duration-300 ease-in-out hover:bg-black/30 dark:bg-white/20 dark:text-white dark:hover:bg-white/30"
-                  >
-                    Github Repo
-                    <FaGithub className="text-[16px] font-bold text-white" />
-                  </Link>
-                </span>
-                </div>
-                
-              </div>
-            </div>
-            <div className="item">
-              <Image
-                width={1920}
-                height={1080}
-                alt="bg_image"
-                src="/images/web3.webp"
-              />
-              <div className="overlay absolute left-0 top-0 z-0 h-full w-full bg-black opacity-[0.9]"></div>
-
-              <div className="content top-[23%] scale-90  md:top-[45.5%]  md:max-w-[80%] lg:left-[6.5%] lg:top-[49%] lg:w-[500px] lg:scale-100">
-                <div className=" max-w-lg">
-                <p>Web Development</p>
-                <h2>Movie (UI CLONE)</h2>
-                <p>
-                A sleek video streaming platform inspired by Netflix, offering a seamless and secure experience for discovering and enjoying your favorite movies and TV shows.
-                </p>
-                <span className="mt-4 flex items-center justify-start gap-4 space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0">
-                  <Link
-                  target="_blank"
-                    href="https://vibrant-kirch-c658a7.netlify.app/"
-                    className="rounded-full bg-lime px-3 py-2 text-[16px] font-semibold text-white hover:opacity-[0.9]"
-                  >
-                    Live Demo
-                  </Link>
-                  <Link
-                  target="_blank"
-                    href="https://github.com/al3x991/TheMovieApp"
-                    className="flex items-center gap-2 rounded-full bg-black/20 px-3 py-2 text-[16px] font-semibold text-black duration-300 ease-in-out hover:bg-black/30 dark:bg-white/20 dark:text-white dark:hover:bg-white/30"
-                  >
-                    Github Repo
-                    <FaGithub className="text-[16px] font-bold text-white" />
-                  </Link>
-                </span>
-                </div>
-                
-              </div>
-            </div>
-            <div className="item">
-              <Image
-                width={1920}
-                height={1080}
-                alt="bg_image"
-                src="/images/web4.webp"
-              />
-              <div className="overlay absolute left-0 top-0 z-0 h-full w-full bg-black opacity-[0.9]"></div>
-
-              <div className="content top-[23%] scale-90  md:top-[45.5%]  md:max-w-[80%] lg:left-[6.5%] lg:top-[49%] lg:w-[500px] lg:scale-100">
-                <div className=" max-w-lg">
-                <p>Web Development</p>
-                <h2>ShopNow</h2>
-                <p>
-                 A web3 crypto investment and trading platform built using the Next.js framework. The application provides a seamless and secure environment for users to connect their Bitcoin, Ethereum, or Solana wallets and start trading.
-                </p>
-                <span className="mt-4 flex items-center justify-start gap-4 space-y-2 sm:flex-row sm:space-x-4 sm:space-y-0">
-                  <Link
-                  target="_blank"
-                    href="https://sad-tereshkova-ee89e8.netlify.app/"
-                    className="rounded-full bg-lime px-3 py-2 text-[16px] font-semibold text-white hover:opacity-[0.9]"
-                  >
-                    Live Demo
-                  </Link>
-                  <Link
-                  target="_blank"
-                    href="https://github.com/al3x991/ShopNow/tree/master"
-                    className="flex items-center gap-2 rounded-full bg-black/20 px-3 py-2 text-[16px] font-semibold text-black duration-300 ease-in-out hover:bg-black/30 dark:bg-white/20 dark:text-white dark:hover:bg-white/30"
-                  >
-                    Github Repo
-                    <FaGithub className="text-[16px] font-bold text-white" />
-                  </Link>
-                </span>
-                </div>
-                
-              </div>
-            </div>
-            {/* Additional slider items */}
+          </motion.div>
+        ))}
+        <button className="prev" onClick={handlePrev}>&lt;</button>
+        <button className="next" onClick={handleNext}>&gt;</button>
+      </div>
+      <div className="thumbnail-container">
+        {slides.map((slide, index) => (
+          <div
+            key={index}
+            className={`thumbnail ${currentIndex === index ? 'active' : ''}`}
+            onClick={() => handleThumbnailClick(index)}
+          >
+            <Image
+              src={slide.src}
+              width={150}
+              height={100}
+              alt={`Thumbnail ${index}`}
+            />
           </div>
-          <div className="arrows absolute bottom-[3%] right-[38%] md:bottom-[10%] md:right-[50px] ">
-            <button id="prev">&lt;</button>
-            <button id="next">&gt;</button>
-          </div>
-          <div className="thumbnail lg:absolute bottom-[-1050px] h-[100%] lg:bottom-[-935px] flex flex-col items-center justify-start relative gap-x-[30px] gap-y-[30px] overflow-y-scroll pb-[80px]  md:flex-row md:items-baseline lg:flex-wrap lg:justify-start lg:pb-[0px] ">
-            <div className="item active md:h-[120px] md:max-h-[70%] md:w-[156px] md:max-w-[70%] ">
-              <Image
-                width={250}
-                height={180}
-                alt="bg_image"
-                src="/images/web01.webp"
-              />
-              <div className="content font-bold text-[#816816]">
-                TitanTTrade
-              </div>
-            </div>
-            <div className="item md:h-[120px] md:max-h-[70%] md:w-[156px] md:max-w-[70%] ">
-              <Image
-                width={250}
-                height={180}
-                alt="bg_image"
-                src="/images/web2.webp"
-              />
-              <div className="content font-bold text-[#816816]">
-                EthosMatrix
-              </div>
-            </div>
-            <div className="item md:h-[120px] md:max-h-[70%] md:w-[156px] md:max-w-[70%] ">
-              <Image
-                width={250}
-                height={180}
-                alt="bg_image"
-                src="/images/web3.webp"
-              />
-              <div className="content font-bold text-[#816816]">
-                MovieStream
-              </div>
-            </div>
-            <div className="item md:h-[120px] md:max-h-[70%] md:w-[156px] md:max-w-[70%] ">
-              <Image
-                width={250}
-                height={180}
-                alt="bg_image"
-                src="/images/web4.webp"
-              />
-              <div className="content font-bold text-[#816816]">
-                ShopNow-Now
-              </div>
-            </div>
-            {/* Additional thumbnails */}
-          </div>
-        </div>
-      </section>
-      <Script strategy="beforeInteractive" src="./scripts/app.js" />
-    </>
+        ))}
+      </div>
+    </section>
   );
 };
 
